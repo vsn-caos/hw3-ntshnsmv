630373b87de77169fed50dd2ed92c99115255ef0
Natalia Anisimova <89299019+ntshnsmv@users.noreply.github.com>
2026-06-19 20:28:02 +0300

    update problem0
    Implemented memory-mapped file handling to search for substring occurrences efficiently.

--- problem0-mmap-find-substrings/solution.c
+++ problem0-mmap-find-substrings/solution.c
@@ -21,6 +21,54 @@ int main(int argc, char *argv[]) {
     // TODO: откройте файл, получите его размер через fstat,
     //       отобразите в память через mmap,
     //       найдите все вхождения argv[2] и выведите их позиции
+    const char *filename = argv[1];
+    const char *needle = argv[2];
+    size_t needle_len = strlen(needle);
+
+    if (needle_len == 0) {
+        return 0;
+    }
+
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        perror("fstat");
+        close(fd);
+        return 1;
+    }
+
+    if (st.st_size == 0 || (size_t)st.st_size < needle_len) {
+        close(fd);
+        return 0;
+    }
+
+    size_t file_size = (size_t)st.st_size;
+
+    char *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        return 1;
+    }
+
+    for (size_t i = 0; i + needle_len <= file_size; ++i) {
+        if (memcmp(data + i, needle, needle_len) == 0) {
+            printf("%zu\n", i);
+        }
+    }
+
+    if (munmap(data, file_size) == -1) {
+        perror("munmap");
+        close(fd);
+        return 1;
+    }
+
+    close(fd);
 
     return 0;
 }
